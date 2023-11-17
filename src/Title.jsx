59b18df6878e992a5f2e7aec87a227c209f13c0a
Salman Ramzan <salman.ramzan23@gmail.com>
2023-11-18 00:20:58 +0500

    Title.jsx

--- src/Title.jsx
+++ src/Title.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Title = ({img, name, job}) => {
 
-  let myImg = 'src/assets/salmanramzan.jpeg', myName = 'Salman Ramzan', myJob = 'Frontend Developer (React)'
+  let myImg = '/src/assets/salmanramzan.jpeg', myName = 'Salman Ramzan', myJob = 'Frontend Developer (React)'
   return (
      <>
         <center>
-          {img && <img src={myImg} width='90' height='90' />}
+          {img || <img src={myImg} width='90' height='90' />}
           <h1 className='name'>{name || myName}</h1>
           <hr />
           <p className='job'>{job || myJob}</p>
